a07e4b003e5f940a51ecb51390911dd674c98841
javierjsa <javierjsa@gmail.com>
2016-12-04 21:42:56 +0100

    Muestreo del optimo

--- LinearDiffPSNR.m
+++ LinearDiffPSNR.m
@@ -1,60 +1,59 @@
-function [data,datan]=LinearDiffPSNR(fn, lambda, dt, val)
-[m,n]=size(fn);
-Nit=length(val);
-datan=zeros(1,Nit);
-data=zeros(1,Nit);
+function [data,datan]=LinearDiffPSNR(original,fn, lambda, dt, start,stop,step,param)
+%function [data,datan]=LinearDiffPSNR(original,fn, lambda, dt, start,stop,step)
+original=im2double(original);
 fn=im2double(fn);
-imlambda=zeros(m,n,Nit);
-imnlambda=zeros(m,n,Nit);
+[m,n]=size(fn);
+Nit=start:step:stop;
+datan=zeros(1,length(Nit));
+data=zeros(1,length(Nit));
+imlambda=zeros(m,n,length(Nit));
+imnlambda=zeros(m,n,length(Nit));
 
 
-for i=1:Nit   
-    it=val(i);
+for i=1:length(Nit)   
+    it=Nit(i);
     [un, diff_un]=LinearDiffusion2016(fn, 0, dt, it);
     imnlambda(:,:,i)=un;
-    %un=im2double(un);
-    %prevn=im2double(un);    
-    valorn=PSNR_V(un,fn);  
-    datan(i)=valorn;    
+    
+    valorn=PSNR_V(un,original);  
+    datan(1,i)=valorn;    
     
     [u, diff_u]=LinearDiffusion2016(fn, lambda, dt, it);
     imlambda(:,:,i)=im2uint8(double(u));
-    %u=im2double(u);
-    %prev=im2double(u);    
-    valor=PSNR_V(u,fn);  
-    data(i)=valor;
+      
+    valor=PSNR_V(u,original);  
+    data(1,i)=valor;
 end  
-%un=double(un);
-%u=double(u);
-%un=im2uint8(un);
-%u=im2uint8(u);
-%{
-subplot(4,1,1);  
-plot(data);
-title('Difusion')
+fprintf('\tPSNR nolambda\tPSNR lambda\t\n');
+fprintf('--------------------------------------------------\n');
+for j=1:length(Nit)
+    fprintf('%d\t%f\t%f\n',Nit(j),datan(1,j),data(1,j));    
+end
+maxl=find(data==max(data));
+maxnl=find(datan==max(datan));
+fprintf('--------------------------------------------------\n');
+fprintf('Max lambda\t%d\t%f\n',Nit(maxl),max(data)); 
+fprintf('Max nolambda\t%d\t%f\n',Nit(maxnl),max(datan));
 
-subplot(4,1,3); 
+figure(1);
+subplot(2,1,1);  
 plot(datan);
-title('Filtrado (lambda=0)');
-%}
-figure;
-title('Con lambda');
-for i=1:Nit
-    subplot(1,Nit,i);  
-    img=uint8(im2double(imlambda(:,:,i)));
-    imshow(img);
-    %imshow(fn);
-    %subplot(2,Nit,i);
-    %imshow(imnlambda(:,:,i));    
+title('No lambda');
+
+subplot(2,1,2); 
+plot(data);
+title('lambda');
+if(param=='y')
+    [un, diff_un]=LinearDiffusion2016(fn, 0, dt, maxnl);
+    [u, diff_u]=LinearDiffusion2016(fn, lambda, dt, maxl);
+
+    figure(2);
+    imshow(im2double(un));
+    title('No lambda');
+
+    figure(3) 
+    imshow(im2double(u));
+    title('lambda');
 end
 
-figure;
-title('Sin lambda');
-for i=1:Nit
-    subplot(1,Nit,i);  
-    img=uint8(im2double(imnlambda(:,:,i)));
-    imshow(img);
-    %imshow(fn);
-    %subplot(2,Nit,i);
-    %imshow(imnlambda(:,:,i));    
-end
+
