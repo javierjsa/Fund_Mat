5ae70ec40b7831a1fab3baabc0afb237f40a79b5
JSA <javierjsa@gmail.com>
2016-12-05 11:20:34 +0100

    LinearDiffPSNR.m terminado

--- LinearDiffPSNR.m
+++ LinearDiffPSNR.m
@@ -1,4 +1,6 @@
 function [data,datan]=LinearDiffPSNR(original,fn, lambda, dt, start,stop,step,param)
+%Realiza un proceso de difusion pura y denoising dentro del rango de
+%iteraciones start-step-stop. Genera un grafico y selecciona le maximo.
 %function [data,datan]=LinearDiffPSNR(original,fn, lambda, dt, start,stop,step)
 original=im2double(original);
 fn=im2double(fn);
@@ -9,7 +11,7 @@ data=zeros(1,length(Nit));
 imlambda=zeros(m,n,length(Nit));
 imnlambda=zeros(m,n,length(Nit));
 
-
+clc;
 for i=1:length(Nit)   
     it=Nit(i);
     [un, diff_un]=LinearDiffusion2016(fn, 0, dt, it);
@@ -24,36 +26,47 @@ for i=1:length(Nit)
     valor=PSNR_V(u,original);  
     data(1,i)=valor;
 end  
-fprintf('\tPSNR nolambda\tPSNR lambda\t\n');
+fprintf('\n Iter\tPSNR difusión\tPSNR denoise\t\n');
 fprintf('--------------------------------------------------\n');
 for j=1:length(Nit)
     fprintf('%d\t%f\t%f\n',Nit(j),datan(1,j),data(1,j));    
 end
 maxl=find(data==max(data));
 maxnl=find(datan==max(datan));
+
+fprintf('\n\t\tK\tPSNR\n');
 fprintf('--------------------------------------------------\n');
-fprintf('Max lambda\t%d\t%f\n',Nit(maxl),max(data)); 
-fprintf('Max nolambda\t%d\t%f\n',Nit(maxnl),max(datan));
+fprintf('Max denoise\t%d\t%f\n',Nit(maxl),max(data)); 
+fprintf('Max difusión\t%d\t%f\n',Nit(maxnl),max(datan));
 
 figure(1);
 subplot(2,1,1);  
-plot(datan);
-title('No lambda');
+plot(Nit,datan);
+title('Difusión');
 
 subplot(2,1,2); 
-plot(data);
-title('lambda');
+plot(Nit,data);
+title('Denoise');
+
+axH = findall(gcf,'type','axes');
+
+if max(data)>max(datan)
+    set(axH,'ylim',[min(datan), max(data)+0.5]);
+else
+    set(axH,'ylim',[min(datan), max(datan)+0.5]);
+end
+
 if(param=='y')
+    figure(2);
     [un, diff_un]=LinearDiffusion2016(fn, 0, dt, maxnl);
     [u, diff_u]=LinearDiffusion2016(fn, lambda, dt, maxl);
-
-    figure(2);
+    subplot(1,2,1);   
     imshow(im2double(un));
-    title('No lambda');
-
-    figure(3) 
+    title('Difusión');
+    
+    subplot(1,2,2);
     imshow(im2double(u));
-    title('lambda');
+    title('Denoise');
 end
 
 
