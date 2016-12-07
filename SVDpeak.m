a3197c83df323483379ecdfb34cf8ba2e1fd11c2
JSA <javierjsa@gmail.com>
2016-12-07 09:57:48 +0100

    Mejora SVSpeak

--- SVDpeak.m
+++ SVDpeak.m
@@ -1,9 +1,11 @@
 function SVDpeak(ruidosa,original)
-%Calcula la descomposicion SVD.
-%Calcula pnsr entre la imagen original y reconstruccion ruidosa
+%SVDpeak(ruidosa,original)
+% 
+%Calcula la descomposicion SVD de la imagen original y la ruidosa.
+%Calcula psnr (max snr) entre la imagen original y reconstruccion ruidosa
 %empleando la salida de la funcion nativa psnr de matlab [peaksnr,snr]:
-% -tomando el valor maximo peaksnr que facilita la funcion
-% -acumulando los valores snr
+% -Tomando el valor maximo peaksnr que facilita la funcion
+% -Acumulando los valores snr
 %Calcula la psnr empleando la función externa PSNR_V
 %Calcula la variacion de la PSNR al reconstruir tanto la imagen
 %original como la ruidosa empleando PSNR_V.
@@ -17,11 +19,13 @@ ruidosa=im2double(ruidosa);
 
 [m,n]=size(S);
 
-%relacion reconstruida original y reconstruida ruidosa
+%PSNR reconstruida original y reconstruida ruidosa
 PSNRratio=zeros(1,m); 
-%relacion reconstruida original y ruidosa
+%PSNR (psnr Matlab)  original y reconstruida ruidosa
 PSNR=zeros(1,m);
-%relacion  original y reconstruida ruidosa PSNRV
+%PSNR (peak psnr Matlab) original y reconstruida ruidosa
+PSNR1=zeros(1,m);
+%PSNR (PSNR_V)  original y reconstruida ruidosa
 PSNR2=zeros(1,m);
 for k=1:m
    rec=U(:,1:k)*S(1:k,1:k)*V(:,1:k)';
@@ -34,17 +38,22 @@ for k=1:m
    
    [actual,val]=psnr(rec,original);
    PSNR(1,k)=val;
+   PSNR1(1,k)=actual;
 end   
 
-subplot(3,1,1);  
+subplot(4,1,1);  
 plot(PSNR);
 title('Función psnr matlab (original, reconst.ruidosa)');
 
-subplot(3,1,2); 
+subplot(4,1,2);  
+plot(PSNR1);
+title('Función psnr (peak snr) matlab (original, reconst.ruidosa)');
+
+subplot(4,1,3); 
 plot(PSNR2);
 title('Función PSNR\_V (original, reconst.ruidosa)');
 
-subplot(3,1,3); 
+subplot(4,1,4); 
 plot(PSNRratio);
 title('Función PSNR\_V (reconst. orig, reconst. ruidosa)');
 
@@ -55,13 +64,21 @@ vmax= max([max(PSNR),max(PSNR2),max(PSNRratio)]);
 
 set(axH,'ylim',[vmin, vmax+0.5]);
 
-
-
 res=max(PSNR);
 res2=max(PSNR2);
+res1=max(PSNR1);
 
-val1=find(PSNR==res);
+val=find(PSNR==res);
 val2=find(PSNR2==res2);
-fprintf('Función psnr(valor directo peaksnr) Matlab:%f\n',actual);
-fprintf('Función psnr(snr) Matlab:%f, K=%d\n',res,val1);
+val1=find(PSNR1==res1);
+
+original_v=PSNR_V(ruidosa,original);
+[original_m,valref]=psnr(rec,original);
+fprintf('Valores de referencia imágenes de partida:\n');
+fprintf(' -Función PSNR_V:%f\n',original_v);
+fprintf(' -Función psnr (valor directo peaksnr) Matlab: %f\n',original_m);
+fprintf('------------------------------------------------------\n');
+
+fprintf('Función psnr(valor directo peaksnr) Matlab:%f, K=%d\n',res,val1);
+fprintf('Función psnr(snr) Matlab:%f, K=%d\n',res,val);
 fprintf('Función PSNR_V:%f, K=%d\n',res2,val2);
