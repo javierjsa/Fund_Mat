42381b5f5fa95ac19c70f58c9b18a1cba0a29f53
JSA <javierjsa@gmail.com>
2016-12-09 13:20:37 +0100

    analisis funciona

--- analisis.m
+++ analisis.m
@@ -1,75 +1,91 @@
 function analisis(A,b)
-%analisis( A,b )
-%Realiza la discusión de un sistema
-%La dimensión de Im( A ) es r, rango de A . Si r=min(n,m) el sistema tiene 
-%solución para todo b . Si r<min(n.m) sólo existe solución si b ∈Im( A ).
-%Warning
+%analisis(A,b)
+%Realiza la discusión de un sistema de ecuaciones lineas
 clc;
 [m,n]=size(A);
+rankA=rank(A);
+rankAb=rank([A b]);
+kernel=n-rank(A);
 
-rank_A=rank(A);
-rank_Ab=rank([A b]);
-
-if (rank_A ~= rank_Ab)
-  comp=0;  
-  str='Rango A distinto de Rango Ab. Sistema incompatible.';
-  else if (rank_A <m && rank_A<n)
-        comp=2;      
-        str='Hay vectores linealmente dependientes';
-       else
-        str='Rango A = Rango A|b. Sistema compatible.';
-        comp=1;
-       end
+M=A'*A;
+bm=A'*b;
+e=eig(M);
+meig=min(e);
+maeig=max(e);
+fprintf('=======================================================\n');
+fprintf('Dimensión Imagen F: %d\n',rankA);
+fprintf('Dimensión espacio de llegada: %d\n',n);
+fprintf('Dimensión kernel: %d\n',kernel); 
+fprintf('=======================================================\n\n');
+if (rankA==rankAb) && (kernel==0)
+    x=A\b;
+    r=(A*x)-b;
+    fprintf('=======================================================\n');
+    fprintf('Rango A|b= Dim Imagen  y kernel=0\n');
+    fprintf('Sistema compatible determinado\n');  
+    fprintf('=======================================================\n');
+    fprintf('Solución del sistema: ');
+    imprimir(x); 
+    fprintf('-------------------------------------------------------\n');
+    fprintf('Residuo Ax-b: ');
+    imprimir(r);
+    
 end
 
-fprintf('Dimensión imagen de A: %i.\n',rank_A);
-fprintf('Rango matriz ampliada: %i.\n',rank_Ab);
-fprintf('%s\n',str);
-if comp==1 || comp==2
-    if n>rank_A
-        fprintf('Dimension del kernel: %i.\n',(n-rank_A));
-        fprintf('-------------------------------\n');
-        fprintf('No hay solución única.\n');
-        %Cálculo pinv
-        x=pinv(A)*b;
-        fprintf('-------------------------------\n');
-        fprintf('\nSolución pinv(A)*b:\n');
-        imprimir(x);
-        fprintf('Resíduo:\n');
-        res=b-(A*x);
-        imprimir(res);
-        disp(norm(res));
-        %Cálculo backlash
-        if comp ~=2
-            xb=A\b;
-            fprintf('\nSolución A\\b:\n');
-            imprimir(xb);
-            fprintf('Resíduo:\n');
-            resb=b-(A*xb);
-            imprimir(resb);
-            disp(norm(resb));
-        end
-        
-    else
-        fprintf('Dimension del kernel: %i\n',(n-rank_A));
-        fprintf('-------------------------------\n');
-        fprintf('Solución única\n');
-        x=A\b;
-        fprintf('-------------------------------\n');
-        fprintf('\nSolución A\\b:');
-        imprimir(x);
-        fprintf('\nResíduo:');
-        res=(A*x)-b;
-        imprimir(res);
-        disp(norm(res));
+
+fprintf('=======================================================\n');
+fprintf('AUTOVALOR MÍNIMO A''A: %d\n',meig);
+fprintf('AUTOVALOR MÁXIMO A''A: %d\n',maeig);
+fprintf('CONDICIONAMIENTO A''A: %d\n',maeig/meig);
+fprintf('=======================================================\n\n');
+if rankA==rankAb && kernel>0
+    fprintf('=======================================================\n');
+    fprintf('Sistema compatible indeterminado\n');
+    if meig>1.e-3         
+        x=M\bm; 
+        r=(A*x)-b;        
+        fprintf('Solución única de mínimos cuadrados A''A=A''b\n');
+        ffprintf('---------------------------------------------------\n');
+        fprintf('Solución del sistema:\n');
+        imprimir(x);    
+        fprintf('Residuo sistema inicial Ax=b:\n');
+        imprimir(r);
+        op_pinv(A,b);
+    else        
+        fprintf('A''A semidefinida positiva\n');
+        fprintf('=======================================================\n\n');
+        fprintf('\nSolución de mínima energía (A''A+Id)x=A''b\n');        
+        op_pinv(A,b);
     end    
 end
-
+    
+if (rankA<rankAb)        
+        fprintf('=======================================================\n');
+        fprintf('Sistema incompatible\n');    
+        fprintf('\nSolución de mínima energía (A''A+Id)x=A''b\n');
+        fprintf('=======================================================\n');    
+        op_pinv(A,b);   
+       
+end
 end
 
 function imprimir (x)
+ fprintf('[ ');
  for i=1:length(x)
-     fprintf('%d\t',x(i));
+     fprintf('%2.3E ',x(i));
  end
+ fprintf(']');
  fprintf('\n');
+end 
+
+function op_pinv(A,b)
+ xpinv=pinv(A)*b;
+ fprintf('Solución operador pinv: ');
+ imprimir(xpinv);
+ fprintf('Energía solución pinv: %f\n',norm(xpinv));
+ fprintf('-------------------------------------------------------\n');
+ fprintf('Residuo pinv Ax-b: ');
+ imprimir((A*xpinv)-b);
+ fprintf('Energía resíduo pinv Ax-b: %2.3E\n',norm((A*xpinv)-b));
+ fprintf('-------------------------------------------------------\n')
 end 
