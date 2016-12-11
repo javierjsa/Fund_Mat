c3c25a47d4bfc2cfb740d8a84f2812318cddefb8
JSA <javierjsa@gmail.com>
2016-12-11 14:44:26 +0100

    Mejoras

--- derivativesRGB.m
+++ derivativesRGB.m
@@ -1,22 +1,31 @@
-function derivatesRGB(img)
+function derivativesRGB(img)
 %derivatesRGB(img)
 %calcula el modulo de una imagen en color y muestra el resultado
 %junto con la imagen en color
 
+clc;
+close all;
+
 imgd=double(img);
 [vg,a,ppg]=colorgrad(imgd);
 
-
+[m,n,j]=size(img);
+ratio=m/n;
 
 figure(1);
 colormap('gray');
 a1=subplot(1,2,1);
-imageshow(img);
+imshow(img);
 title(a1,'Imagen');
-pbaspect(a1,[1 1 1]);
+pbaspect(a1,[1 ratio 1]);
+set(gca,'XTick',[]);
+set(gca,'YTick',[]);
 b1=subplot(1,2,2);
 imagesc(im2double(vg));
 title(b1,'Modulo del gradiente');
-pbaspect(b1,[1 1 1]);
+pbaspect(b1,[1 ratio 1]);
+set(gca,'XTick',[]);
+set(gca,'YTick',[]);
+
 
 end
